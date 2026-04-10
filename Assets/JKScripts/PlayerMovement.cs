6ed1f04662fd1dd9e677a6b37dbdace2173a4236
kikiblueberries3 <kikiblueberries3@gmail.com>
2026-04-10 17:20:15 -0400

    Kitchen and Cameras
    I've created the kitchen model, added a player mock character movements (but you can put your character in separately), created trigger cameras to change between portions of the kitchen, and I started to add utensil assets into the game to begin interactive object creation.

---
+++ Assets/JKScripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 3f;
+    public float turnSpeed = 180f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       controller = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movDir;
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        movDir = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        controller.Move(movDir * Time.deltaTime - Vector3.up * 0.1f);
+    }
+}
